08cb11d74833a784ee482cc56faaa2bd6ab3ae9d
Wolfgang-PA <Wolfgang-PA@DESKTOP-AQS8IHQ.fritz.box>
2016-01-12 22:48:20 +0100

    Erste Anwendung

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Test extends Application {
@@ -8,8 +11,20 @@ public class Test extends Application {
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage stage) throws Exception {
+
+			
+		Button b = new Button("Servus");
+		HBox h = new HBox(b);
+		h.setMaxHeight(100);
+		b.setMaxSize(80, 50);
+		Scene s = new Scene(h, 300, 200);
+		
+		stage.setScene(s);
+		stage.show();
+		
+		
+		
 		
 	}
 
