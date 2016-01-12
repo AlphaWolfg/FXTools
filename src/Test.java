609db87a7004183d79eae0805abe3aaf03681aed
Wolfgang-PA <Wolfgang-PA@DESKTOP-AQS8IHQ.fritz.box>
2016-01-12 22:34:28 +0100

    Initialize a Java Project

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+	
+	public static void main(String[] args) {
+		Application.launch();
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
